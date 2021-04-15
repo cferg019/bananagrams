d1280a4559bc0830bc0b29badc8dc5682fd466de
Casey Ferguson <cferg019@gmail.com>
2021-04-14 20:47:21 -0400

    basic express app set up

---
+++ back-end/src/app.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+
+const express = require('express')
+const logger = require('morgan')
+
+const app = express()
+const PORT = process.env.PORT
+
+app.use(logger('dev'))
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+const postRouter = require('./routes/posts')
+
+app.use('/posts', postRouter)
+
+app.listen(PORT, () => {
+    console.info(`app listening on port ${PORT}`)
+})
+
