8bd436ee31e1e50c5fe6c842441453c64e633f47
Casey Ferguson <cferg019@gmail.com>
2021-04-16 17:30:34 -0400

    database set-up - BAN-1

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -2,6 +2,16 @@ require('dotenv').config()
 
 const express = require('express')
 const logger = require('morgan')
+const mongoose = require('mongoose')
+
+const dbConnString = process.env.DB_CONN_STRING
+
+mongoose.connect(dbConnString, {useNewUrlParser: true, useUnifiedTopology: true})
+const db = mongoose.connection
+db.on('error', err => console.error('DB connection error', err))
+db.once('open',function() {
+    console.log('connected boyeeee')
+})
 
 const app = express()
 const PORT = process.env.PORT
@@ -18,4 +28,3 @@ app.use('/posts', postRouter)
 app.listen(PORT, () => {
     console.info(`app listening on port ${PORT}`)
 })
-
